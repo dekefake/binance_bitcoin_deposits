1e65f29f3bcfb89e1d9985dde4f8349090427878
Thomas <desathomas@gmail.com>
2023-05-10 17:44:13 +0200

    Add Wallet-Chat Widget from https://github.com/Wallet-Chat/widgetdemo/blob/main/README.md

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import TransactionsList from './TransactionsList';
 
+import {WalletChatWidget} from 'react-wallet-chat-v0'
+import 'react-wallet-chat-v0/dist/index.css'
+
 const binance_address = '34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo';
 
 function App() {
@@ -10,6 +13,7 @@ function App() {
         <h1>Binance BTC deposits to {binance_address}</h1>
       </header>
       <TransactionsList address={binance_address}/>
+      <WalletChatWidget />
     </div>
   );
 }
